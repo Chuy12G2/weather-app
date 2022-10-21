09041b96b39f6293c8dc2aa3ef82b98485f1f6f7
Jesus <chuygs12@gmail.com>
2022-10-21 16:32:57 -0500

    add some styling

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const searchInput = document.getElementById("search-field");
 const searchButton = document.getElementById("search-button");
 const temperaturePara = document.getElementById("temperature");
-let place = "";
-let fetchInput = ""; 
+const countryPara = document.querySelector(".country");
+const cityPara = document.querySelector(".city");
 
 searchButton.addEventListener("click", () => {
-    place = searchInput.value;
-    fetchInput = `http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=6fc7cb1dbd7015b387d3ead3e4fcd889`;
-    callAPI();
-})
+  let place = searchInput.value;
+  let fetchInput = `http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=6fc7cb1dbd7015b387d3ead3e4fcd889`;
+  callAPI(fetchInput);
+});
 
-
-
-async function callAPI(){
-    const response = await fetch(fetchInput, {mode: "cors"});
+async function callAPI(fetchInput) {
+  try {
+    const response = await fetch(fetchInput, { mode: "cors" });
 
     const responseData = await response.json();
     console.log(responseData);
 
-    const temperature = await responseData.main.temp;
+    const temperature = responseData.main.temp;
     temperaturePara.innerHTML = temperature + " F";
+
+    let country = responseData.sys.country;
+    countryPara.textContent = country;
+
+    let city = responseData.name;
+    cityPara.textContent = city;
+  } catch (error) {
+    alert("City not found, try again");
+  }
 }
-
-
-
